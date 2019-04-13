54da3fef4a5f8f498b5cfa8cf80ab7bda13cf1f2
Timbo <5blaesche@informatik.uni-hamburg.de>
2019-04-13 12:01:35 +0200

    Adds a env variable for mysql

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,6 +1,6 @@
 export const PORT = process.env.PORT || 3001;
 
-export const MYSQL_PORT = 3306;
+export const MYSQL_PORT = process.env.MYSQLCONNSTR_localdb || 3306;
 
 export const MYSQL_USER = process.env.MYSQL_USER;
 
