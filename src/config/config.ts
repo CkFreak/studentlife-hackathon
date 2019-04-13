502f6a2670b6b1ed3176cbb4224adf3bfe741e62
Timbo <5blaesche@informatik.uni-hamburg.de>
2019-04-13 12:30:35 +0200

    Changes ports and usernames

--- src/config/config.ts
+++ src/config/config.ts
@@ -2,11 +2,11 @@ export const PORT = process.env.PORT || 3001;
 
 export const MYSQL_HOST = process.env.MYSQLCONNSTR_localdb || "localhost";
 
-export const MYSQL_PORT = 3306;
+export const MYSQL_PORT = 50681;
 
-export const MYSQL_USER = process.env.MYSQL_USER;
+export const MYSQL_USER = process.env.MYSQL_USER || "root";
 
-export const MYSQL_PASSWORD = process.env.MYSQL_PASSWORD;
+export const MYSQL_PASSWORD = process.env.MYSQL_PASSWORD || "";
 
 export const SCORES_TABLE = "scores";
 
